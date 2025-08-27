ae5bf17b187ca1d4dfa843d67923078c95e2e137
Kurt Sanor <sanorkurtkelly@gmail.com>
2025-08-27 14:38:03 +0800

    fix dao

--- src/main/java/org/example/controller/UserController.java
+++ src/main/java/org/example/controller/UserController.java
@@ -12,7 +12,7 @@ import java.sql.SQLException;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = "https://motorphpayroll.netlify.app/", allowCredentials = "true")
+@CrossOrigin(origins = "https://motorphpayroll.netlify.app", allowCredentials = "true")
 @RequestMapping("/users")
 public class UserController {
     @Autowired
