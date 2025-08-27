b8b7a693782e4e2b7628a0aeebee2ab6d9f5b3ca
Kurt Sanor <sanorkurtkelly@gmail.com>
2025-08-27 17:22:07 +0800

    fixed attendance repo

--- src/main/java/org/example/repository/AttendanceRepository.java
+++ src/main/java/org/example/repository/AttendanceRepository.java
@@ -10,6 +10,9 @@ import java.util.List;
 public interface AttendanceRepository extends JpaRepository<Attendance, Integer> {
     List<Attendance> findByEmployeeId(int id);
 
-    @Query("SELECT a FROM Attendance a WHERE a.employee.id = :id AND FUNCTION('MONTH', a.date) = :month AND FUNCTION('YEAR', a.date) = :year")
+    @Query(
+        value = "SELECT * FROM attendance a WHERE a.employee_id = :id AND EXTRACT(MONTH FROM a.date) = :month AND EXTRACT(YEAR FROM a.date) = :year",
+        nativeQuery = true
+    )
     List<Attendance> findByEmployeeIdAndMonthAndYear(@Param("id") int id, @Param("month") int month, @Param("year") int year);
 }
